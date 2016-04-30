76e0d6bcc1381ab55de11ea8672d69e7161b7d2c
numbers in calculator <tasneem.ahmed131@gmail.com>
2016-04-30 23:30:01 +0200

    adding last row& events

--- src/proj1/project1.java
+++ src/proj1/project1.java
@@ -13,7 +13,7 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
-public class project1  extends Application{
+public class project1 extends Application{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -23,14 +23,14 @@ public class project1  extends Application{
 	Button [] arr = new Button[10];
 	TextField text = new TextField();
 	String res="";
-	 static int size=0;   //ÕÃ„ «·«—Ì «·ÃœÌœÂ › ﬂ· „—Â »Õ”» «·ﬂ”»—‘‰ 
+	 static int size=0;   //??? C?C?? C??I?I? ? ?? ??? E??E C???E?O? 
 	@Override
 	public void start(Stage st) throws Exception {
 		text.setPrefHeight(70);
 		text.setPrefWidth(300);
 		GridPane g = new  GridPane();
 		g.add(text,0,0,6,2);	
-		/// «·“—«Ì— „‰ Ê«Õœ ·  ”⁄…
+		/// C???C?? ?? ?C?I ? E??E
 		for(int i =1; i<10 ;i++){
 			  arr[i]= new Button(Integer.toString(i));
 	    	  
@@ -41,6 +41,39 @@ public class project1  extends Application{
 			     for(int i =2 ;i<=4 ;i++)
 			    	 for(int j=2;j>=0;j--)
 			    		g.add(arr[k--], j, i);
+			  // adding markes
+			      
+			     
+			     Button []arr2= new Button[10];
+			     for(int i =0; i<10 ;i++){	
+			    	  arr2[i]= new Button();
+			    	  arr2[i].setPrefSize(50,40);
+			     } 
+			     int a=0;
+			        for(int i =2 ;i<=4 ;i++){
+			       	 for(int j=3;j<=5;j++)
+			       	g.add(arr2[a++], j, i); 
+			       	 
+			       	 
+			        }
+			        arr2[0].setText("/");
+			        arr2[1].setText("DEL");         
+			        arr2[2].setText("AC");       
+			        arr2[3].setText("*");
+			        arr2[4].setText("(");
+			        arr2[5].setText(")");
+			        arr2[6].setText("-");
+			        arr2[7].setText("x^2");       
+			        arr2[8].setText("Sqrt");         
+			    ///////////////////////////////////////////////////////////////////////////////////////////////////		     
+			       // «last row
+			        String [] str = {"0",".", "%" ,"+"};
+			        Button []last_row= new Button[4];
+			       	 for(int i=0;i<4;i++){
+			       		 last_row[i]= new Button(str[i]);
+			       		  last_row[i].setPrefSize(50,40); 
+			       				 g.add(last_row[i], i, 5);	
+			       	 }
 			       g.setHgap(3);
                    g.setVgap(3);		
 		st.setScene(new Scene(g,300,200));
@@ -84,5 +117,55 @@ public class project1  extends Application{
 		text.setText((text.getText()+arr[9].getText()));
 		
 		});
-	}}
+	arr2[0].setOnAction(e->{
+		text.setText((text.getText()+" " +arr2[0].getText()+" "));
 	
+	});
+
+	arr2[1].setOnAction(e->{
+		String remind= text.getText();
+		
+		      if(Character.isDigit(remind.charAt(remind.length()-1)))
+				text.setText(remind.substring(0, remind.length()-1));
+		      
+		      else
+		    	  text.setText(remind.substring(0, remind.length()-3));  
+	
+	});		
+	arr2[2].setOnAction(e->{
+		text.setText("");
+	
+	});
+
+	arr2[3].setOnAction(e->{
+		text.setText((text.getText()+" "+arr2[3].getText()+" "));
+	
+	});
+
+	arr2[4].setOnAction(e->{
+		text.setText((text.getText()+arr2[4].getText())+" ");
+	
+	});
+
+	arr2[5].setOnAction(e->{
+		text.setText((text.getText()+" " +arr2[5].getText()));
+	
+	});
+
+	arr2[6].setOnAction(e->{
+		text.setText((text.getText()+" " +arr2[6].getText()+" "));
+	
+	});
+
+	arr2[7].setOnAction(e->{
+		text.setText((text.getText()+" " +"^ 2"));
+	
+	});
+
+	arr2[8].setOnAction(e->{
+		text.setText((text.getText()+" " +arr2[8].getText()+" "));
+	
+	});
+	  
+     
+	}}
